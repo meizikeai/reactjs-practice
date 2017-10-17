6a0a6fe0f2563cbdbce4e5c245860b87a0aac4e3
爱得太迟 <meizikeai@163.com>
2017-10-17 16:17:04 +0800

    Updata
    find a girlfriend...

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -32,106 +32,92 @@ export default class Cart extends Component {
             let result = this.state.info.data;
 
             return <div className="app-cart">
-                <div className="bundle">
-                    <div className="shop" data-reactid="">
-                        <div className="title">
-                            <div className="shopcb">
-                                <p>
-                                    <input id="cb-150813973007821" className="o-t-cb" type="checkbox" />
-                                    <label htmlFor="cb-150813973007821"></label>
-                                </p>
-                            </div>
-                            <div className="ico">
-                                <span className="shopIco_B"></span>
-                            </div>
-                            <div className="contact">
-                                <a href="//shop.m.taobao.com/shop/shop_index.htm?shop_id=61773004">
-                                    <p className="title">veromoda官方旗舰</p>
-                                    <p className="arrow">
-                                        <span className="icon-right"></span>
+                <div className="cart-thing">
+                    <div className="bundle">
+                        <div className="shop" data-reactid="">
+                            <div className="shop-title">
+                                <div className="check">
+                                    <p>
+                                        <input className="checkbox" id="cb-150813973007821" type="checkbox" />
+                                        <label htmlFor="cb-150813973007821"></label>
                                     </p>
-                                </a>
-                            </div>
-                            <div className="state">
-                                <div className="state-cont">
+                                </div>
+                                <div className="ico">
+                                    <span></span>
+                                </div>
+                                <div className="contact">
+                                    <a href="//shop.m.taobao.com/shop/shop_index.htm?shop_id=61773004">
+                                        <p className="title">veromoda官方旗舰</p>
+                                        <p className="arrow"></p>
+                                    </a>
+                                </div>
+                                <div className="state">
                                     <p className="edit">编辑</p>
                                 </div>
                             </div>
-                        </div>
-                        <div className="coudan">
-                            <div className="ctrl-ui-label">
+                            <div className="coudan">
                                 <div className="icon">
                                     <img src="//img.alicdn.com/tfs/TB1GulZSVXXXXaWXVXXXXXXXXXX-129-36.png" alt="" />
                                 </div>
-                                <div className="box full limitHeight">
-                                    <div className="title">
-                                        <div>
-                                            <h2 className="titleSub">满2件,享包邮</h2>
-                                        </div>
-                                    </div>
+                                <div className="box">
+                                    <div className="title">满2件,享包邮</div>
                                     <span></span>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="group ">
-                        <div className="itemv2 edit-false">
-                            <div className="item-box" data-status="open">
-                                <div className="item-list o-t-item undefined">
-                                    <div className="item-cb">
-                                        <p>
-                                            <input id="cb-150813973008024" type="checkbox" className="cb o-t-cb" />
-                                            <label htmlFor="cb-150813973008024"></label>
-                                        </p>
-                                    </div>
-                                    <div className="item-detail">
-                                        <div>
-                                            <div className="item-img">
+                        <div className="group ">
+                            <div className="each">
+                                <div className="scheme" data-status="open">
+                                    <div className="content">
+                                        <div className="check">
+                                            <p>
+                                                <input className="checkbox" id="cb-150813973008024" type="checkbox" />
+                                                <label htmlFor="cb-150813973008024"></label>
+                                            </p>
+                                        </div>
+                                        <div className="detail">
+                                            <div className="picture">
                                                 <a href="//a.m.taobao.com/i551881638423.htm">
-                                                    <img className="" src="https://gw.alicdn.com/bao/uploaded/i3/420567757/TB2BWhyXTAKh1JjSZFDXXbKlFXa-420567757.jpg_200x200q50s150.jpg_.webp" alt="" data-src-checked="true" />
+                                                    <img src="https://gw.alicdn.com/bao/uploaded/i3/420567757/TB2BWhyXTAKh1JjSZFDXXbKlFXa-420567757.jpg_200x200q50s150.jpg_.webp" alt="" />
                                                 </a>
                                             </div>
-                                            <div className="item-info">
+                                            <div className="info">
                                                 <a href="//a.m.taobao.com/i551881638423.htm">
-                                                    <h3 className="title">Vero Moda2017秋季新品印花A字裙摆七分袖连衣裙|31737C505</h3>
+                                                    <h3>Vero Moda2017秋季新品印花A字裙摆七分袖连衣裙|31737C505</h3>
                                                     <div className="sku">
                                                         <p>E39深蓝色;155/76A/XS</p>
                                                     </div>
                                                 </a>
-                                                <div className="item-logos"></div>
                                                 <div className="pay">
-                                                    <div className="pay-price">
-                                                        <div className="price">
-                                                            <p className="o-t-price" data-symbol="￥">
-                                                                <span>
-                                                                    <span className="major">699</span>
-                                                                    <span className="point">.</span>
-                                                                    <span className="minor">00</span>
-                                                                </span>
-                                                            </p>
+                                                    <div className="price">
+                                                        <div className="sale">
+                                                            <span>￥</span>
+                                                            <span className="major">699</span>
+                                                            <span className="point">.</span>
+                                                            <span className="minor">00</span>
+                                                        </div>
+                                                        <div className="origin">
+                                                            <del>￥269.00</del>
                                                         </div>
                                                     </div>
                                                     <div className="quantity">
-                                                        <p>
-                                                            <span>x</span>
-                                                            <span>1</span>
-                                                        </p>
+                                                        <span>x</span>
+                                                        <span>1</span>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
+                                        <div className="del edit-del">
+                                            <p>删除</p>
+                                        </div>
                                     </div>
-                                    <div className="item-del c-edit-delhide">
+                                </div>
+                                <div className="option hide">
+                                    <div className="del edit-del">
                                         <p>删除</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="op">
-                                <div className="item-del c-edit-delhide">
-                                    <p>删除</p>
-                                </div>
-                            </div>
-                            <div className="op2"></div>
                         </div>
                     </div>
                 </div>
